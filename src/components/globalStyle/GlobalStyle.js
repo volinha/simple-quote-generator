264b42f110526a5cb9e8f25a117358bb0e621a52
Vinícius Fernandes <viniciusfernandesmct@gmail.com>
2022-01-12 01:20:08 -0300

    created: globalstyle

--- src/components/globalStyle/GlobalStyle.js
+++ src/components/globalStyle/GlobalStyle.js
@@ -1,10 +1,12 @@
 import { createGlobalStyle } from "styled-components";
+import background from '../../images/2725551.jpg'
 
 export const GlobalStyle = createGlobalStyle`
 body {
+    background: url(${background}) center no-repeat;
+    background-size: cover;
     padding: 0;
     margin: 0;
     color:  #332c36;
     font-family: 'Roboto Mono', Roboto, sans-serif;
-}
-`;
+}`;
